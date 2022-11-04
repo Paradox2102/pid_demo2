4c355897515d560f19226e573b00642cc4db9fd5
bovlb <31326650+bovlb@users.noreply.github.com>
2022-11-03 21:42:08 -0700

    Tidy up the code a bit

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -1,8 +1,10 @@
 import math
+from multiprocessing.sharedctypes import Value
 
 from time import time, sleep
 from typing import NamedTuple
 import threading
+from collections import defaultdict
 
 from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, Slider, Button, Span
@@ -27,21 +29,17 @@ def stop_profiling():
     pr.disable()
     print('profiling disabled')
 
-class Config(NamedTuple):
-    p: float
-    i: float
-    d: float
-    izone: float
-    f: float
-    setpoint: float
-    ratio: int
-
 
 class PID:
-    def __init__(self):
+    def __init__(self, p=0, i=0, d=0, izone=0, setpoint=0):
         self.last_err = None
         self.err_acc = 0
         self.continuous = False
+        self.p = p
+        self.i = i
+        self.d = d
+        self.izone = izone
+        self.setpoint = setpoint
         
     def enable_continuous_input(self, minimum_input, maximum_input):
         self.continuous = True
@@ -49,9 +47,9 @@ class PID:
         self.maximum_input = maximum_input
         self.error_bound = (maximum_input - minimum_input) / 2.0
         
-    def calculate(self, measurement, config:Config, dt):
-        err = self._calculate_difference(config.setpoint - measurement)
-        p_output = config.p * err
+    def calculate(self, measurement, dt):
+        err = self._calculate_difference(self.setpoint - measurement)
+        p_output = self.p * err
         if dt is not None:
             assert self.last_err is not None, f"dt={dt}"
             d_err = self._calculate_difference(err - self.last_err) / dt
@@ -59,15 +57,15 @@ class PID:
             d_err = 0
         self.last_err = err
 
-        d_output = config.d * d_err
+        d_output = self.d * d_err
 
         #print(dict(dt=dt, d_err=d_err, err=err, err_acc=self.err_acc))
         
-        if abs(err) < config.izone and dt is not None and config.i > 0:
+        if abs(err) < self.izone and dt is not None and self.i > 0:
             self.err_acc += err * dt
         else:
             self.err_acc = 0
-        i_output = config.i * self.err_acc
+        i_output = self.i * self.err_acc
         return dict(p=p_output, d=d_output, i=i_output)
     
     def _calculate_difference(self, value):
@@ -75,6 +73,31 @@ class PID:
             return input_modulus(value, -self.error_bound, +self.error_bound)
         else:
             return value
+
+    def set_p(self, value):
+        self.p = value
+
+    def set_i(self, value):
+        self.i = value
+
+    def set_d(self, value):
+        self.d = value
+
+    def set_izone(self, value):
+        self.izone = value
+
+    def set_setpoint(self, value):
+        measurement = self.setpoint - self.last_err
+        self.setpoint = value
+        self.last_err = self._calculate_difference(self.setpoint - measurement)
+
+    def reset(self):
+        self.last_err = None
+
+    @property
+    def columns(self):
+        return ['p', 'i', 'd']
+
 
 # Following three methods are cribbed from:
 # https://first.wpi.edu/wpilib/allwpilib/docs/release/java/src-html/edu/wpi/first/math/MathUtil.html
@@ -128,6 +151,9 @@ class Gearbox:
         torque = self.motor.calculate(velocity / self.ratio, output)
         return torque * self.ratio
 
+    def set_ratio(self, value):
+        self.ratio = value
+
 
 g = 10
 
@@ -154,60 +180,92 @@ class ModelArm(Model):
 
 
 class Process:
-    def __init__(self, config):
+    def __init__(self, f=0):
         self.last_time = time()
         self.start = time()
         motor = Motor(inertia=0.5, torque=12.0)
         motor = Gearbox(motor, 20)
-        self.reset(config)
         self.model = ModelArm(mass=0.1, length=1.0, motor=motor) 
+        self.f = f
         self.ff = ff_arm
-        self.dilation = 1
         self.output = 0
-
-    def update(self, config):
-        now = time()
-        dt = (now - self.last_time) * self.dilation
-        self.last_time = now
-        (self.position, self.velocity, acceleration) = self.model.calculate(position=self.position, velocity=self.velocity, dt=dt, output=self.output)
-        output = self.pid.calculate(measurement=self.position, config=config, dt=dt)
-        output['f'] = self.ff(config.f, config.setpoint)
-        output['total'] = sum(output.values())
-        self.output = clamp(output['total'], -1, +1)
-        self.model.motor.ratio = config.ratio
-        return ((now-self.start) * self.dilation, self.position, self.velocity, acceleration, output)
-
-    def reset(self, config):
-        self.position = -90
-        self.velocity = 0
         self.pid = PID()
         self.pid.enable_continuous_input(-180, 180)
-        self.pid.calculate(measurement=self.position, config=config, dt=None)
+        self.voltage = 12
+        self.reset()
+
+    def update(self):
+        now = time()
+        dt = now - self.last_time
+        self.last_time = now
+        (self.position, self.velocity, acceleration) = self.model.calculate(position=self.position, velocity=self.velocity, dt=dt, output=self.output)
+        output = self.pid.calculate(measurement=self.position, dt=dt)        
+        output['f'] = self.ff(self.f, self.pid.setpoint)
+        result = {
+            'voltage_' + key: clamp(value, -1, 1) * self.voltage
+            for key, value in output.items()
+        }
+        self.output = clamp(sum(output.values()), -1, 1)
+        result['voltage'] =  self.output * self.voltage
+        result['position'] = self.position
+        result['velocity'] = self.velocity
+        result['acceleration'] = acceleration
+        result['ts'] = now - self.start
+        return result
+
+    def reset(self):
+        self.position = -90
+        self.velocity = 0
+        self.pid.reset()
+        self.pid.calculate(measurement=self.position, dt=None)
+
+    def set_f(self, value):
+        self.f = value
+
+    @property
+    def columns(self):
+        return ['voltage', 'position', 'velocity', 'acceleration', 'ts', 'voltage_f'] + [
+            'voltage_' + key for key in self.pid.columns
+        ]
 
 
-def get_empty_data():
+def get_empty_data(process, controls):
+    return { key: [] for key in (process.columns + list(controls.keys())) }
+
+
+data = None
+
+def make_controls():
     return dict(
-        ts=[],
-        setpoint=[],
-        position=[],
-        velocity=[],
-        acceleration=[],
-        voltage=[],
-        voltage_p=[],
-        voltage_i=[],
-        voltage_d=[],
-        voltage_f=[],
-        p=[],
-        i=[],
-        d=[],
-        f=[],
+        p=Slider(start=0., end=0.1, value=0., step=0.001, title="p", sizing_mode="stretch_width", format='0.000'),
+        f=Slider(start=0., end=1, value=0., step=0.005, title="f", sizing_mode="stretch_width", format='0.000'),
+        i=Slider(start=0., end=0.1, value=0., step=0.001, title="i", sizing_mode="stretch_width", format='0.000'),
+        d=Slider(start=0., end=0.2, value=0., step=0.001, title="d", sizing_mode="stretch_width", format='0.000'),
+        izone=Slider(start=0., end=90, value=20., step=5, title="izone", sizing_mode="stretch_width"),
+        setpoint=Slider(start=-180., end=180, value=0., title="setpoint", sizing_mode="stretch_width"),
+        ratio=Slider(start=1, end=100, value=10, title="gear ratio", sizing_mode="stretch_width"),
     )
 
-data = get_empty_data()
+
+def connect_controls(process, controls):
+    controls['p'].on_change("value", lambda attr, old, new: process.pid.set_p(new))
+    controls['i'].on_change("value", lambda attr, old, new: process.pid.set_i(new))
+    controls['d'].on_change("value", lambda attr, old, new: process.pid.set_d(new))
+    controls['f'].on_change("value", lambda attr, old, new: process.set_f(new))
+    controls['izone'].on_change("value", lambda attr, old, new: process.pid.set_izone(new))
+    controls['setpoint'].on_change("value", lambda attr, old, new: process.pid.set_setpoint(new))
+    controls['ratio'].on_change("value", lambda attr, old, new: process.model.motor.set_ratio(new))
+
 
 def bkapp(doc):
+    process = Process()
+    controls = make_controls()
+    connect_controls(process, controls)
 
-    source = ColumnDataSource(get_empty_data())
+    global data
+    data = get_empty_data(process, controls)
+
+    source = ColumnDataSource(data)
     animation_source =ColumnDataSource(dict(
         setpoint_x=[],
         setpoint_y=[],
@@ -272,65 +330,36 @@ def bkapp(doc):
     p3.toolbar.logo = None
     p3.toolbar_location = None   
 
-    p_widget = Slider(start=0., end=0.1, value=0., step=0.001, title="p", sizing_mode="stretch_width", format='0.000')
-    f_widget = Slider(start=0., end=1, value=0., step=0.005, title="f", sizing_mode="stretch_width", format='0.000')
-    i_widget = Slider(start=0., end=0.1, value=0., step=0.001, title="i", sizing_mode="stretch_width", format='0.000')
-    d_widget = Slider(start=0., end=0.2, value=0., step=0.001, title="d", sizing_mode="stretch_width", format='0.000')
-    izone_widget = Slider(start=0., end=90, value=20., step=5, title="izone", sizing_mode="stretch_width")
-    setpoint_widget = Slider(start=-180., end=180, value=0., title="setpoint", sizing_mode="stretch_width")
-    ratio_widget = Slider(start=1, end=100, value=10, title="gear ratio", sizing_mode="stretch_width")
-
-    def get_config():
-        return Config(
-            p=p_widget.value,
-            f=f_widget.value,
-            i=i_widget.value,
-            d=d_widget.value,
-            izone=izone_widget.value,
-            setpoint=setpoint_widget.value,
-            ratio=ratio_widget.value,
-        )
-                
-    process = Process(get_config())
 
     reset_button = Button(label="Reset Arm", sizing_mode="stretch_width")
     def reset():
-        process.reset(get_config())
+        process.reset()
     reset_button.on_click(reset)
 
     reflect_button = Button(label="Reflect Setpoint", sizing_mode="stretch_width")
     def reflect():
-        setpoint_widget.value = input_modulus(180 - setpoint_widget.value, -180, 180)
+        controls['setpoint'].value = input_modulus(180 - controls['setpoint'].value, -180, 180)
     reflect_button.on_click(reflect)
 
     #@linear()
     def update_data():
         global data
-        config = get_config()
-        (ts, position, velocity, acceleration, output) = process.update(config)
-        data['ts'].append(ts)
-        data['position'].append(position)
-        data['velocity'].append(velocity)
-        data['acceleration'].append(acceleration)
-        data['setpoint'].append(config.setpoint)
-        data['voltage'].append(clamp(output['total'], -1, 1) * 12)
-        data['voltage_p'].append(clamp(output['p'], -1, 1) * 12)
-        data['voltage_i'].append(clamp(output['i'], -1, 1) * 12)
-        data['voltage_d'].append(clamp(output['d'], -1, 1) * 12)
-        data['voltage_f'].append(clamp(output['f'], -1, 1) * 12)
-        data['p'].append(config.p)
-        data['i'].append(config.i)
-        data['d'].append(config.d)
-        data['f'].append(config.f)
+        for key, widget in controls.items():
+            data[key].append(widget.value)
+
+        process_result = process.update()
+        for key, value in process_result.items():
+            data[key].append(value)
+
 
     def update_animation(setpoint, position):
         setpoint_rad = setpoint * math.pi / 180
         position_rad = position * math.pi / 180
         animation_data = dict(
-            setpoint_x=[math.cos(setpoint * math.pi / 180)],
-            setpoint_y=[math.sin(setpoint * math.pi / 180)],
-            position_x=[math.cos(position * math.pi / 180)],
-            position_y=[math.sin(position * math.pi / 180)]
+            setpoint_x=[math.cos(setpoint_rad)],
+            setpoint_y=[math.sin(setpoint_rad)],
+            position_x=[math.cos(position_rad)],
+            position_y=[math.sin(position_rad)]
         )        
         animation_source.stream(animation_data, 1)
 
@@ -340,13 +369,14 @@ def bkapp(doc):
         if len(data['ts']) > 0:
             source.stream(data, int(update_frequency*window))
             update_animation(data['setpoint'][-1], data['position'][-1])
-            data = get_empty_data()
+            data = get_empty_data(process, controls)
         
+    controls_column = column(*(controls[x] for x in ['f', 'p', 'i', 'izone', 'd', 'setpoint', 'ratio']), sizing_mode="stretch_width")
 
     doc.add_root(
             column(
                 row(
-                    column(f_widget, p_widget, i_widget, izone_widget, d_widget, setpoint_widget, ratio_widget, sizing_mode="stretch_width"), 
+                    controls_column, 
                     column(p3, reset_button, reflect_button, sizing_mode="fixed"), 
                     sizing_mode="stretch_width"
                 ), 
